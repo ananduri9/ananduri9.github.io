1e83ac8be98374ab1bbd2610249620f9d72f8049
Anand Nanduri <an9878@gmail.com>
2017-12-28 22:30:17 -0500

    finished all except text content

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //NavBar functionality
-
 function changeColor(obj) {
 	$('.nav').css({color:"white"});
 	obj.style.color="blue";
 }
 
 $(document).ready(function(){
-    $(".nav").mouseleave(function(){
-  		$('.nav').css({color:"white"});
+    $(".nav a").mouseleave(function(){
+  		$('.nav a').css({color:"white"});
     });
 });
 
@@ -18,3 +17,17 @@ $(document).ready(function(){
         $(this).animate({top: '-=2px'});
     });
 });
+
+
+//Smooth Scrolling
+$(document).ready(function(){
+	$('.nav a').click(function(){
+		var hash = this.hash;
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 1100, function(){
+			window.location.hash=hash;
+		});
+	});
+})
